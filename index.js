eb54824cb215f09e61de38af495fc6a631d0b05e
Monica  Karasek <mlkarasek@gmail.com>
2018-09-11 22:02:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,28 +6,28 @@ class BoardMember {
   }
 
   sayVeto() {
-    return `No, I must disagree`return
+    return 'No, I must disagree'
   }
 
   sayApprove() {
-    return `You can do that!`
+    return 'You can do that!'
   }
 
   sayDoCharity() {
-    return `I like to help people.`
+    return 'I like to help people.'
   }
 
   sayReleasePressStatement() {
-    return `You will see great things from Scuber.`
+    return 'You will see great things from Scuber.'
   }
 
   sayHi() {
-    return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`
+    return 'Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.'
   };
 }
 
 class Ceo extends BoardMember {
   hireEmployee() {
-    return `Welcome aboard!`
+    return 'Welcome aboard!'
   }
 }
